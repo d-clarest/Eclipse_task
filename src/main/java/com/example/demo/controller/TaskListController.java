41673e17a57d3b337c160ba1d5d86c51da1981fd
d-clarest <d.clarest.gpt@gmail.com>
2025-06-22 04:24:43 +0900

    微修正

---
+++ src/main/java/com/example/demo/controller/TaskListController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class TaskListController {
+	
+//	private final TaskListService service;
+	
+	 
+	
+
+}
