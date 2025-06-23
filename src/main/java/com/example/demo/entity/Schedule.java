bd9efb80ddd962822609cb63c48c404e54436e1f
d-clarest <d.clarest.gpt@gmail.com>
2025-06-23 20:54:54 +0900

    htmlに予定を追加，entityにもろんぼっくを追加しただけ．

---
+++ src/main/java/com/example/demo/entity/Schedule.java
@@ -0,0 +1,20 @@
+package com.example.demo.entity;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import lombok.Data;
+
+@Data
+public class Schedule {
+    private boolean addFlag;         // 予定追加 ON/OFF
+    private String title;            // 予定名
+    private String dayOfWeek;        // 曜日
+    private LocalDate scheduleDate;  // 予定日
+    private LocalTime startTime;     // 開始時刻
+    private LocalTime endTime;       // 終了時刻
+    private String location;         // 場所
+    private String detail;           // 詳細
+    private int point;               // ポイント
+    private LocalDate completedDay;  // 完了日（null 可）
+}
