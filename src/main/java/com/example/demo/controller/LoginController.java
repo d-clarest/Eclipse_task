42b64e04ddaaaabf56398e1aad651d68efca3a7f
d-clarest <d.clarest.gpt@gmail.com>
2025-06-25 19:02:57 +0900

    Update task top path to include username

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -45,18 +45,18 @@ public class LoginController {
 	
     @PostMapping("/task-top")
     public String showTop(LogInForm form, Model model) {
-            User u = new User();
-            u.setUsername(form.getUsername());
-            u.setPassword(form.getPassword());
-            log.debug("Attempting login for user: {}", form.getUsername());
-            if (loginService.login(u)) {
-                    log.debug("Login success for user: {}", form.getUsername());
-                    return "redirect:/task-top";
-            } else {
-                    log.debug("Login failed for user: {}", form.getUsername());
-                    model.addAttribute("errorMessage", "ログイン失敗");
-                    return "log-in";
-            }
+        User u = new User();
+        u.setUsername(form.getUsername());
+        u.setPassword(form.getPassword());
+        log.debug("Attempting login for user: {}", form.getUsername());
+        if (loginService.login(u)) {
+            log.debug("Login success for user: {}", form.getUsername());
+            return "redirect:/" + form.getUsername() + "/task-top";
+        } else {
+            log.debug("Login failed for user: {}", form.getUsername());
+            model.addAttribute("errorMessage", "ログイン失敗");
+            return "log-in";
+        }
     }
 }
 
