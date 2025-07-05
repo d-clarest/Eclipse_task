105d33f48ee110e66ca7bfdefad5bfaa9f12cbaf
d-clarest <d.clarest.gpt@gmail.com>
2025-07-05 23:58:26 +0900

    Remove unused form and clean LogInForm

--- src/main/java/com/example/demo/form/LogInForm.java
+++ src/main/java/com/example/demo/form/LogInForm.java
@@ -4,26 +4,6 @@ import lombok.Data;
 
 @Data
 public class LogInForm {
-//	@NotNull(message="入力してください。")
-//	@Min(value=1,  message="正の整数を入力してください。")
-//	private Integer restaurantId;
-//	
-//	private String restaurantName;
-//
-//	@Size(min=4, max=16, message="4文字から16文字で指定してください。")
-//	private String userId;
-//
-//	@Past(message="今日以前の日付を入力してください。")
-//	private Date visitDate;
-//
-//	@NotNull(message="入力してください。")
-//	@Min(value=1, message="1-5で指定してください。")
-//	@Max(value=5, message="1-5で指定してください。")
-//	private Integer rating;
-//
-//	@Size(min=1, max=128, message="1文字から128文字で指定してください。")
-//	private String comment;
-	private String username;
-	private String password;
-	private int userid;
+    private String username;
+    private String password;
 }
